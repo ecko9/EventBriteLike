2dbd586024fcf4173006e258b20f84c330872e04
ecko9 <jean.ferstler@gmail.com>
2021-08-09 21:18:00 +0200

    e

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_LOGIN'],
+  :api_key => ENV['SENDGRID_LOGIN'],
   :password => ENV['SENDGRID_PWD'],
   :domain => 'bidon.fr',
   :address => 'smtp.sendgrid.net',
