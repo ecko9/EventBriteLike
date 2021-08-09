ce378572c6d255d65d018554d02f6381442adb19
ecko9 <jean.ferstler@gmail.com>
2021-08-09 19:53:24 +0200

    Mailer files

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,26 @@ class Event < ApplicationRecord
 
   has_many :participations
   has_many :participants, through: :participations, class_name: "User"
+
+  validates :start_date, presence: true, :start_date_need_be_positive
+  validates :duration, presence: true, :duration_need_be_multiple_of_five_and_positive
+  validates :title, presence: true, length: { in: 5..140 }
+  validates :description, presence: true, length: { in: 20..1000 }
+  validates :price, presence: true, numericality: { only_integer: true, less_than: 1001, greater_than: 0 }
+  validates :location, presence: true
+
+
+  def start_date_need_be_positive
+    if start_date.present? && start_date < Date.today
+      errors.add(:start_date, "can't be in the past")
+    end
+  end
+
+  def duration_need_be_multiple_of_five_and_positive
+    if (duration % 5) != 0 && duration < 0
+      errors.add(:duration, "need be multiple of 5 and positive")
+    end
+  end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
+
   has_many :events, foreign_key: 'administrator_id'
 
   has_many :participations, foreign_key: 'participant_id'
   has_many :events, through: :participations
+
+  validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Donnez un mail valide" }
+  validates :first_name, presence: true, length: { in: 1..25 }
+  validates :last_name, presence: true, length: { in: 1..25 }
+  validates :description, length: { in: 5..300 }
+
+
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
